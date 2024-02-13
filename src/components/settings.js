eea07282b22b0c8c8d25e6e1c7d1bcbd81d82c96
harshrai-11 <harsh789rai@gmail.com>
2024-02-13 16:50:39 +0530

    show tags data for edit and delete

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button, Stack } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
-import { ADD_TAGS } from "../routes";
+import { ADD_TAGS, GET_TAG_LIST } from "../routes";
 import { Loader } from "../layout/loader";
-import { PostApiHeaders } from "../utils.js/constant";
+import { ApiHeaders, PostApiHeaders } from "../utils.js/constant";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Signup = () => {
   const [tags, setTags] = useState("");
   const [isLoading, setIsloading] = useState(false);
+  const [tagList, setTagList] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    getTags().catch((error) => {
+      console.log("get tags error", error);
+    });
+  }, []);
+
+  const getTags = async () => {
+    const resp = await fetch(GET_TAG_LIST, ApiHeaders);
+    const result = await resp.json();
+    setTagList(result);
+  };
+
+  function createData(adName, spend) {
+    return { adName, spend };
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -35,6 +57,7 @@ const Signup = () => {
     <React.Fragment>
       <div className="settings">
         {isLoading && <Loader />}
+        {/* <div> */}
         <h1 style={{ color: "white" }}>Add Category For Post</h1>
         <form onSubmit={handleSubmit} action={<Link to="/" />}>
           <Stack spacing={2} sx={{ marginBottom: 4 }}>
@@ -51,7 +74,55 @@ const Signup = () => {
             Add Category
           </Button>
         </form>
+
+        <div>
+          <TableContainer component={Paper}>
+            <Table sx={{ width: 150 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <strong>Categories</strong>
+                  </TableCell>
+                  {/* <TableCell align="right">
+                    <strong>Total Spend</strong>
+                  </TableCell> */}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tagList?.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.tag}
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={() => {}}
+                      >
+                        Edit
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="outlined"
+                        color="error"
+                        onClick={() => {}}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
+      {/* </div> */}
     </React.Fragment>
   );
 };
